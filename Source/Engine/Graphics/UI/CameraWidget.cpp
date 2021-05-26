ac8a61c9964fd5ff0801e21be483e70b3ef9f72a
JasperDre <jasperdre@gmail.com>
2021-05-26 21:22:24 +0200

    Update CameraWidget.cpp

--- Source/Engine/Graphics/UI/CameraWidget.cpp
+++ Source/Engine/Graphics/UI/CameraWidget.cpp
@@ -16,12 +16,12 @@ void UI::CameraWidget::Tick()
 	Engine& engine = Engine::GetInstance();
 	World& world = *engine.GetWorld();
 	Entity& camera = world.GetCamera();
-	CameraComponent* cameraComponent = camera.GetComponent<CameraComponent>();
+	CameraComponent& cameraComponent = camera.GetComponent<CameraComponent>();
 
-	ImGui::Text("Front x:%0.f y:%0.f z:%0.f", cameraComponent->GetFront().x, cameraComponent->GetFront().y, cameraComponent->GetFront().z);
-	ImGui::Text("Up x:%0.f y:%0.f z:%0.f", cameraComponent->GetUp().x, cameraComponent->GetUp().y, cameraComponent->GetUp().z);
-	ImGui::Text("Right x:%0.f y:%0.f z:%0.f", cameraComponent->GetRight().x, cameraComponent->GetRight().y, cameraComponent->GetRight().z);
-	ImGui::Text("FOV: %0.f", cameraComponent->GetFieldOfView());
-	ImGui::Text("Position x:%f y:%f z:%f", cameraComponent->GetPosition().x, cameraComponent->GetPosition().y, cameraComponent->GetPosition().z);
-	ImGui::Text("Direction x:%f y:%f z:%f", cameraComponent->GetDirection().x, cameraComponent->GetDirection().y, cameraComponent->GetDirection().z);
+	ImGui::Text("Front x:%0.f y:%0.f z:%0.f", cameraComponent.GetFront().x, cameraComponent.GetFront().y, cameraComponent.GetFront().z);
+	ImGui::Text("Up x:%0.f y:%0.f z:%0.f", cameraComponent.GetUp().x, cameraComponent.GetUp().y, cameraComponent.GetUp().z);
+	ImGui::Text("Right x:%0.f y:%0.f z:%0.f", cameraComponent.GetRight().x, cameraComponent.GetRight().y, cameraComponent.GetRight().z);
+	ImGui::Text("FOV: %0.f", cameraComponent.GetFieldOfView());
+	ImGui::Text("Position x:%f y:%f z:%f", cameraComponent.GetPosition().x, cameraComponent.GetPosition().y, cameraComponent.GetPosition().z);
+	ImGui::Text("Direction x:%f y:%f z:%f", cameraComponent.GetDirection().x, cameraComponent.GetDirection().y, cameraComponent.GetDirection().z);
 }
