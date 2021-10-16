c5a57df59e9b2dbff27d2fe426898d2d60e093d7
Jasper Drescher <jasperdre@gmail.com>
2021-10-16 23:04:10 +0200

    Remove library suffix

--- GenerateVS2019.bat
+++ GenerateVS2019.bat
@@ -1,4 +1,4 @@
 mkdir Generated
 cd Generated
-cmake -DGLFW_USE_HYBRID_HPG:BOOL=ON -Wno-dev -DLIBRARY_SUFFIX="" -G "Visual Studio 16 2019" -Ax64 ..
+cmake -DGLFW_USE_HYBRID_HPG:BOOL=ON -Wno-dev -G "Visual Studio 16 2019" -Ax64 ..
 pause
