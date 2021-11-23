ce4d2914543175f2824c00ad721d3b254aea1cf2
Jasper Drescher <jasperdre@gmail.com>
2021-11-23 20:05:55 +0100

    Separate GLFW and GLAD build step

--- Scripts/InitSubmodules.bat
+++ Scripts/InitSubmodules.bat
@@ -1,1 +1,18 @@
-git submodule update --init --recursive
+@echo on
+echo "Initializing submodules (1/5)"
+:: git submodule update --init --recursive
+mkdir Build
+echo "Generating GLFW (2/5)"
+cd Build
+mkdir GLFW
+cd ..
+cmake -S Dependencies/glfw -B Build/GLFW
+echo "Building GLFW (3/5)"
+cmake --build Build/GLFW --target glfw --config Debug
+echo "Generating GLAD (4/5)"
+cd Build
+mkdir GLAD
+cd ..
+cmake -S Dependencies/glad -B Build/GLAD
+echo "Building GLAD (5/5)"
+cmake --build Build/GLAD --target glad --config Debug
